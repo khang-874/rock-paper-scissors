d2bd515c3e00d98d5291c77c26cfabb69da8e0f4
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-07 16:36:57 -0500

    Create playGround function

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+let choice = ["rock", "paper", "scissors"];
 function getComputerChoice(){
-    return Math.ceil(Math.random() * 3);
+    return choice[Math.ceil(Math.random() * 3) - 1];
 }
 
-for(let i = 0; i < 3; ++i)
-    console.log(getComputerChoice());
+function playGround(playerSelection1, computerSelection){
+    let playerSelection = playerSelection1.toLowerCase();
+    if(playerSelection == computerSelection)
+        return "Tie";
+    if(playerSelection == "rock"){
+        if(computerSelection == "paper")
+            return "You lose! Paper beats Rock";
+        return "You win! Rock beats scissors";
+    }
+    if(playerSelection == "paper"){
+        if(computerSelection == "rock")
+            return "You win! Paper beats Rock";
+        return "You lose! Scissors beats Paper";
+    }
+
+    if(computerSelection == "rock")
+        return "You lose! Rock beats Scissors";
+    return "You win! Scissors beats Paper";
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playGround(playerSelection, computerSelection));
