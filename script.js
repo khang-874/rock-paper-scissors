4c09255bada070b3f4e4bda95b4aeb4ee295a864
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-20 11:03:30 -0500

    Add scoreboard for computer and player

--- script.js
+++ script.js
@@ -30,6 +30,31 @@ function game(playerSelection){
 
 let container = document.querySelector("#container");
 let buttons = [];
+let resultDisplay = document.createElement("div");
+resultDisplay.textContent = "Welcome to Rock Paper Scissors game";
+
+let playerScoreBoard = document.createElement("div");
+let computerScoreBoard = document.createElement("div");
+let playerScore = 0;
+let computerScore = 0;
+playerScoreBoard.textContent = `Your score: ${playerScore}`;
+computerScoreBoard.textContent = `Computer score: ${computerScore}`;
+playerScoreBoard.id = "playerScoreBoard";
+computerScoreBoard.id = "computerScoreBoard";
+
+function displayResult(result){
+    resultDisplay.textContent = result;
+    if(result == "Tie")
+        return;
+    if(result[4] == "w"){
+        playerScore++;
+        playerScoreBoard.textContent = `Your score: ${playerScore}`;
+    }else{
+        computerScore++;
+        computerScoreBoard.textContent = `Computer score: ${computerScore}`;
+    }
+}
+
 for(let i = 0 ; i < 3; ++i){
     buttons.push(document.createElement("button"));
     buttons[i].id = choice[i];
@@ -37,7 +62,12 @@ for(let i = 0 ; i < 3; ++i){
     buttons[i].setAttribute("value", choice[i]);
     buttons[i].textContent = choice[i];
     buttons[i].addEventListener("click", (e) => {
-        console.log(e.target.id);
+        displayResult(game(e.target.id));
+        check();
     })
     container.appendChild(buttons[i]);
 }
+
+container.appendChild(resultDisplay);
+container.appendChild(playerScoreBoard);
+container.appendChild(computerScoreBoard)
